7af7c77c031fc8cdbc4bafc8aea81a47c670e5fb
Jakub Surdej <jakub.sudej@shift4.com>
2024-02-19 17:12:20 +0100

    Create About page

---
+++ src/pages/About/About.test.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import About from "./About";
+
+describe("About Page", () => {
+  test("displays the heading", () => {
+    render(<About />);
+    expect(screen.getByRole("heading")).toHaveTextContent("About Page");
+  });
+
+  test("displays the correct description", () => {
+    render(<About />);
+    expect(screen.getByText(/this is the about page/i)).toBeInTheDocument();
+  });
+});
---
+++ src/pages/About/About.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+        This is the about page of our example application. It provides
+        information about the application and its purpose.
+      </p>
+    </div>
+  );
+};
+
+export default About;
